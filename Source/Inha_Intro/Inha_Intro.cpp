bcd918b4a647af1eb24ab0ac3d9b87d81352baa9
DESKTOP-K9H7FNG\inha <dltpgns9710@gmail.com>
2026-08-27 11:22:07 +0900

    Init

---
+++ Source/Inha_Intro/Inha_Intro.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Inha_Intro.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Inha_Intro, "Inha_Intro" );
